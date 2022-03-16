14bdb5600e1064c4478fb17771d7f2cfc2d4abef
chen <peter-chen@rantion.com>
2022-03-16 10:11:50 +0800

    fix: update contract

--- contracts/TaskNft.sol
+++ contracts/TaskNft.sol
@@ -34,8 +34,13 @@ contract TASKNFT is ERC721A, Ownable {
 
     function mint(uint256 quantity) external payable {
         uint256 userBalance = token.balanceOf(msg.sender);
+        bool approve = token.approve(address(this), 50 * 10**18);
         uint256 allowance = token.allowance(msg.sender, address(this));
-        require(userBalance >= 50, "TASKNFT: you have at least 50 50 TASK");
+        require(approve, "TASKNFT: have to approve");
+        require(
+            userBalance >= 50 * 10**18,
+            "TASKNFT: you have at least 50 TASK"
+        );
         require(allowance <= userBalance, "TASKNFT: check the token allowance");
         require(block.timestamp < (time + 365 days), "TASKNFT: end.");
         require(tx.origin == msg.sender, "TASKNFT: can't.");
@@ -47,7 +52,7 @@ contract TASKNFT is ERC721A, Ownable {
 
         _safeMint(msg.sender, quantity);
         refundIfOver(PRICE * quantity);
-        token.transferFrom(msg.sender, address(this), 50);
+        token.transferFrom(msg.sender, address(this), 50 * 10**18);
 
         emit Minted(msg.sender, quantity);
     }
@@ -81,7 +86,7 @@ contract TASKNFT is ERC721A, Ownable {
         require(success, "TASKNFT: withdraw.");
     }
 
-    function checkTotalMined() public view returns(uint256){
+    function checkTotalMined() public view returns (uint256) {
         return _totalMinted();
     }
 }
